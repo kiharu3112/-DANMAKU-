e3b8914043c0f69ee1c83e6152cca67d20264e5c
kiji-haru <kijiharu3112@gmil.com>
2022-09-30 04:22:41 +0900

    rename font

--- main.rb
+++ main.rb
@@ -7,13 +7,13 @@ Dir[
   'lib/scene/*.rb',
   'lib/fixtures/*.rb',
   'lib/fixtures/result/*.rb',
-  'lib/setting.rb'
+  'lib/font.rb'
 ].each do |file|
   require_relative file
 end
 $PATH = File.dirname(__FILE__)
 $score = 0
-Font.install("lib/fixtures/setting/x8y12pxTheStrongGamer.ttf")
+Font.install("lib/fixtures/font/x8y12pxTheStrongGamer.ttf")
 scenemanager = SceneManager.new
 Window.loop do
   scenemanager.update
