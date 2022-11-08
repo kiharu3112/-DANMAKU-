3130f125474a5f7b99b0f03957a1c7c94300ce63
kiji-haru <kijiharu3112@gmil.com>
2022-11-09 00:24:11 +0900

    fps

--- lib/setting.rb
+++ lib/setting.rb
@@ -7,5 +7,6 @@ module Setting
   DEFAULT_FONT_SIZE = 24
   TITLE_FONT_SIZE = 48
   ENDING_FONT = Font.new(48, 'Westminster')
-  Window.bgcolor = C_BLACK
+  Window.bgcolor = C_WHITE
+  Window.fps = 60
 end
